a33bcb718091eabad2a45bf16076617b804b3ddb
Edson Luiz Zeballos dos Santos Filho <edson.filho@ms.senac.br>
2019-06-26 16:07:06 -0400

    merge com dados do facebook

---
+++ app/config/facebook-auth.js
@@ -0,0 +1,7 @@
+var facebookAuth = {
+    'clientID'        : '415097349334292', // facebook App ID
+    'clientSecret'    : '7f1418870a55ce5a8fbe872f45544856', // facebook App Secret
+    'callbackURL'     : 'http://localhost:4000/auth/facebook/callback'
+};
+
+module.exports = facebookAuth;
